e83ec1272125d42416c3ccf2ff5e68adb9b3deab
Oliver Leonor <oliver.leonor90@gmail.com>
2026-04-18 00:26:43 +0800

    chore(mobile): suppress 3rd-party SafeAreaView deprecation warning

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { LogBox, StyleSheet } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { BellySafeScreen } from './src/screens/BellySafeScreen';
+
+// Silences a deprecation warning emitted from a third-party dep, not our code.
+LogBox.ignoreLogs(['SafeAreaView has been deprecated']);
 
 export default function App() {
   return (
